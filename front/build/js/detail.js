49874d7e01690037b622ceac6ac7af7c6bac1b28
2558772316 <2558772316@qq.com>
2016-06-09 21:49:04 +0800

    finish detail page

--- front/build/js/detail.js
+++ front/build/js/detail.js
@@ -1,7 +1,16 @@
 (function () {
     var m = {
         init: function () {
-
+            m.setScreenHeight();
+            m.resizeWindow();
+        },
+        setScreenHeight: function () {
+            $(".detail-wrap, .detail-img, .detail-buy").height(+$(window).height() - 132);
+        },
+        resizeWindow: function () {
+            $(window).resize(function () {
+                m.setScreenHeight();
+            })
         }
     };
     m.init();
--- front/publish/js/detail.js
+++ front/publish/js/detail.js
@@ -1,1 +1,1 @@
-!function(){var i={init:function(){}};i.init()}();
+!function(){var i={init:function(){i.setScreenHeight(),i.resizeWindow()},setScreenHeight:function(){$(".detail-wrap, .detail-img, .detail-buy").height(+$(window).height()-132)},resizeWindow:function(){$(window).resize(function(){i.setScreenHeight()})}};i.init()}();
