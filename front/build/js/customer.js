5cc10eac057cf6eba07c4a1fcad52d1a9891fbf3
2558772316 <2558772316@qq.com>
2016-07-06 23:32:01 +0800

    debug Merchants api

--- front/build/js/customer.js
+++ front/build/js/customer.js
@@ -8,6 +8,10 @@
         jfdh: true,
         xsxf: false,
         sjzd: false,
+        role: "-1",
+        plist: [],
+        clist: [],
+        alist: [],
         spendpoint: {
             name: "",
             type: "",
@@ -15,19 +19,135 @@
             total: "",
             point: "",
             mobile: ""
+        },
+        total: {},
+        tj: {
+            mobile: "",
+            captcha: "",
+            referrerMobile: ""
+        },
+        tobesaler: {
+            "referrerMobile": "",
+            "storeName": "",
+            "province": "",
+            "provinceCode": "",
+            "city": "",
+            "cityCode": "",
+            "area": "",
+            "areaCode": "",
+            "street": "",
+            "legalPersonIDCardImage": "",//法人身份证
+            "legalPersonWithIDCardInHandImage": "",//法人手持身份证照片
+            "storeAppearance": "",//店招
+            "storeInsideImages": [],//店铺内部图片
+            "license": ""
         }
     };
     var m = {
         init: function () {
             $.checkSession();
             m.getUserInfo();
+            m.imgUpload();
+            m.getProvince();
+        },
+        getProvince: function () {
+            $.when($.ajax({
+                url: $.apiUrl + "/address",
+                type: "GET"
+            })).done(function (d) {
+                $.ylbAjaxHandler(d, function () {
+                    vcustomer.plist = d.data;
+                });
+            });
+        },
+        getCity: function (code) {
+            $.when($.ajax({
+                url: $.apiUrl + "/address?code=" + code,
+                type: "GET"
+            })).done(function (d) {
+                $.ylbAjaxHandler(d, function () {
+                    vcustomer.clist = d.data;
+                });
+            })
+        },
+        getArea: function (code) {
+            $.when($.ajax({
+                url: $.apiUrl + "/address?code=" + code,
+                type: "GET"
+            })).done(function (d) {
+                $.ylbAjaxHandler(d, function () {
+                    vcustomer.alist = d.data;
+                });
+            })
+        },
+        imgUpload: function () {
+            $("#yyzz").dropzone({
+                url: $.apiUrl + "/upload",
+                paramName: "file",
+                maxFiles: 1,
+                maxFilesize: 1.0, // MB
+                acceptedFiles: "image/*",
+                addRemoveLinks: true,
+                dictResponseError: '上传文件出错！',
+                success: function (file, response) {
+                    vcustomer.tobesaler.license = response.data;
+                }
+            });
+            $("#frsfz").dropzone({
+                url: $.apiUrl + "/upload",
+                paramName: "file",
+                maxFiles: 1,
+                maxFilesize: 1.0, // MB
+                acceptedFiles: "image/*",
+                addRemoveLinks: true,
+                dictResponseError: '上传文件出错！',
+                success: function (file, response) {
+                    vcustomer.tobesaler.legalPersonIDCardImage = response.data;
+                }
+            });
+            $("#zmt").dropzone({
+                url: $.apiUrl + "/upload",
+                paramName: "file",
+                maxFiles: 1,
+                maxFilesize: 1.0, // MB
+                acceptedFiles: "image/*",
+                addRemoveLinks: true,
+                dictResponseError: '上传文件出错！',
+                success: function (file, response) {
+                    vcustomer.tobesaler.legalPersonWithIDCardInHandImage = response.data;
+                }
+            });
+            $("#dzt").dropzone({
+                url: $.apiUrl + "/upload",
+                paramName: "file",
+                maxFiles: 1,
+                maxFilesize: 1.0, // MB
+                acceptedFiles: "image/*",
+                addRemoveLinks: true,
+                dictResponseError: '上传文件出错！',
+                success: function (file, response) {
+                    vcustomer.tobesaler.storeAppearance = response.data;
+                }
+            });
+            $("#dnt").dropzone({
+                url: $.apiUrl + "/upload",
+                paramName: "file",
+                maxFiles: 4,
+                maxFilesize: 4.0, // MB
+                acceptedFiles: "image/*",
+                addRemoveLinks: true,
+                dictResponseError: '上传文件出错！',
+                success: function (file, response) {
+                    vcustomer.tobesaler.storeInsideImages.push(response.data);
+                }
+            });
         },
         createQRcode: function () {
             var url = "http://www.hnylbsc.com/sendpoint.html?uid=" + vcustomer.info.mobile;
             $('#customer-private-qrcode').qrcode({
                 render: "table",
-                width: 130,
-                height: 130,
+                width: 140,
+                height: 120,
                 text: url
             });
         },
@@ -38,6 +158,7 @@
             })).done(function (d) {
                 $.ylbAjaxHandler(d, function () {
                     vcustomer.info = d.data;
+                    vcustomer.tj.referrerMobile = d.data.mobile
                     m.getJFDHOrder();
                     //m.buildVue();
                 });
@@ -59,7 +180,7 @@
             $.when($.ajax({
                 url: $.apiUrl + "/user/orders?k=2",
                 type: "GET",
-               // data: JSON.stringify({ k: 2 })
+                // data: JSON.stringify({ k: 2 })
             })).done(function (d) {
                 $.ylbAjaxHandler(d, function () {
                     vcustomer.xsxflist = d.data.orders;
@@ -76,6 +197,18 @@
             })).done(function (d) {
                 $.ylbAjaxHandler(d, function () {
                     vcustomer.sjzdlist = d.data.orders;
+                    m.getTotal();
+                    //m.buildVue();
+                });
+            });
+        },
+        getTotal: function () {
+            $.when($.ajax({
+                url: $.apiUrl + "/user/wage",
+                type: "GET"
+            })).done(function (d) {
+                $.ylbAjaxHandler(d, function () {
+                    vcustomer.total = d.data;
                     m.buildVue();
                 });
             });
@@ -132,6 +265,60 @@
                     },
                     getpoint: function () {
                         this.getPointPerDay();
+                    },
+                    rolechange: function (el) {
+                        vcustomer.role = $(el.target).val();
+                    },
+                    changeprov: function (el) {
+                        var _this = $(el.target);
+                        var v = _this.val();
+                        var t = _this.find("option:selected").text();
+                        vcustomer.tobesaler.provinceCode = v;
+                        vcustomer.tobesaler.province = t;
+                        vcustomer.alist = [];
+                        m.getCity(v);
+                    },
+                    changecity: function (el) {
+                        var _this = $(el.target);
+                        var v = _this.val();
+                        var t = _this.find("option:selected").text();
+                        vcustomer.tobesaler.cityCode = v;
+                        vcustomer.tobesaler.city = t;
+                        m.getArea(v);
+                    },
+                    changearea: function (el) {
+                        var _this = $(el.target);
+                        var v = _this.val();
+                        var t = _this.find("option:selected").text();
+                        vcustomer.tobesaler.areaCode = v;
+                        vcustomer.tobesaler.area = t;
+                    },
+                    apysaler: function () {
+                        $.ajax({
+                            url: $.apiUrl + "/user/merchant",
+                            type: "PUT",
+                            data: JSON.stringify(vcustomer.tobesaler)
+                        }).done(function (d) {
+                            $.ylbAjaxHandler(d, function () {
+                                $.ylbAlert("申请成功！");
+                            });
+                        });
+                    },
+                    logon: function () {
+                        $.ajax({
+                            url: $.apiUrl + "/user/register",
+                            type: "PUT",
+                            data: JSON.stringify(vcustomer.tj)
+                        }).done(function (d) {
+                            $.ylbAjaxHandler(d, function () {
+                                $.ylbAlert("创建成功");
+                                vcustomer.tj = {
+                                    mobile: "",
+                                    captcha: "",
+                                    referrerMobile: ""
+                                };
+                            })
+                        })
                     },
                     //*****ajaxMethod******//
                     getPointPerDay: function () {
