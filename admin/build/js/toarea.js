584fc93f42c08e23acb1b33a01f0d590b9db57c6
2558772316 <2558772316@qq.com>
2016-07-30 19:31:51 +0800

    areamanager sendtaste

--- admin/build/js/toarea.js
+++ admin/build/js/toarea.js
@@ -4,7 +4,48 @@
     };
     var m = {
         init: function () {
-            
+            m.getAreaList();
+        },
+        getAreaList: function () {
+            $.when($.ajax({
+                url: $.apiUrl + "/areamanagers",
+                type: "GET"
+            })).done(function (d) {
+                toarea.list = d.data;
+                m.buildVue();
+            });
+        },
+        buildVue: function () {
+            toarea = new Vue({
+                el: "#toarea-main",
+                data: toarea,
+                methods: {
+                    sends: function (el) {
+                        var _this = $(el.target);
+                        var id = _this.attr("data-id");
+                        var num = _this.prev("input").val();
+                        if (!num || num == "0") {
+                            $.ylbAlert("请填写赠送数额");
+                            return;
+                        } else {
+                            $.ajax({
+                                url: $.apiUrl + "/areamanager/places",
+                                type: "PuT",
+                                data: JSON.stringify({
+                                    userID: id,
+                                    quantity: num
+                                })
+                            }).done(function (d) {
+                                $.ylbAjaxHandler(d, function () {
+                                    $.ylbAlert("赠送成功！");
+                                    m.getAreaList();
+                                });
+                            });
+                        }
+                    }
+                }
+            })
         }
-    }
+    };
+    m.init();
 })();
