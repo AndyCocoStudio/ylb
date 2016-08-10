1ab3f757810c80f29832a0837cbdf1a1823ac095
2558772316 <2558772316@qq.com>
2016-08-10 23:43:36 +0800

    tastepoint page down

--- front/build/js/tastepoint.js
+++ front/build/js/tastepoint.js
@@ -1,10 +1,15 @@
 (function () {
     var taste = {
-        
+        total: {
+            count: [],
+            area: {},
+            customer: {},
+            merchant: {}
+        }
     };
     var m = {
         init: function () {
-            m.getCount();
+            m.getInfo();
         },
         getCount: function () {
             $.when($.ajax({
@@ -12,8 +17,54 @@
                 type: "GET"
             })).done(function (d) {
                 $.ylbAjaxHandler(d, function () {
-                    taste.count = d.data;
-                    m.getInfo();
+                    switch (taste.user.role) {
+                        case "Merchants":
+                            taste.total.count = d.data;
+                            m.merchants();
+                            break;
+                        case "CustomerManager":
+                            taste.total.count = d.data;
+                            m.customerManager();
+                            break;
+                        case "AreaManager":
+                            taste.total.count = d.data;
+                            m.areaManager();
+                            break;
+                        case "CM":
+                            for (var i = 0; i < d.data.length; i++) {
+                                if (d.data[i].role == "CustomerManager") {
+                                    taste.total.customer = d.data[i];
+                                } else {
+                                    taste.total.customer = "";
+                                }
+                                if (d.data[i].role == "Merchant") {
+                                    taste.total.merchant = d.data[i];
+                                } else {
+                                    taste.total.merchant = "";
+                                }
+                            }
+                            m.cm();
+                            break;
+                        case "AM":
+                            for (var i = 0; i < d.data.length; i++) {
+                                if (d.data[i].role == "AreaManager") {
+                                    taste.total.area = d.data[i];
+                                } else {
+                                    taste.total.area = "";
+                                }
+                                if (d.data[i].role == "Merchant") {
+                                    taste.total.merchant = d.data[i];
+                                } else {
+                                    taste.total.merchant = "";
+                                }
+                            }
+                            m.am();
+                            break;
+                        default:
+                            alert("您是普通会员，没有赠送体验积分权限");
+                            window.location.href = "customer.html";
+                            break;
+                    }
                 });
             })
         },
@@ -24,82 +75,78 @@
             })).done(function (d) {
                 $.ylbAjaxHandler(d, function () {
                     taste.user = d.data;
-                    switch (d.data.role) {
-                        case "Merchants":
-                            m.merchants();
-                            break;
-                        case "CustomerManager":
-                            m.customerManager();
-                            break;
-                        case "AreaManager":
-                            m.areaManager();
-                            break;
-                        case "CM":
-                            m.cm();
-                            break;
-                        case "AM":
-                            m.am();
-                            break;
-                        default:
-                            alert("您是普通会员，没有赠送体验积分权限");
-                            window.location.href = "customer.html";
-                            break;
-                    }
+                    m.getCount();
                 });
             });
         },
         merchants: function () {
             $.when($.ajax({
-                url: $.apiUrl + "/merchant/users",
+                url: $.apiUrl + "/merchant/users?cp=1&sz=1000",
                 type: "GET"
             })).done(function (d) {
                 $.ylbAjaxHandler(d, function () {
-                    taste.list = d.data.users;
+                    taste.olist = d.data.users;
                     m.buildVue();
                 });
             });
         },
         customerManager: function () {
             $.when($.ajax({
-                url: $.apiUrl + "/customermanager/merchants",
+                url: $.apiUrl + "/customermanager/merchants?cp=1&sz=1000",
                 type: "GET"
             })).done(function (d) {
                 $.ylbAjaxHandler(d, function () {
-                    taste.list = d.data.merchants;
+                    taste.mlist = d.data.merchants;
                     m.buildVue();
                 });
             });
         },
         areaManager: function () {
             $.when($.ajax({
-                url: $.apiUrl + "/area/customermanager",
+                url: $.apiUrl + "/area/customermanager?cp=1&sz=1000",
                 type: "GET"
             })).done(function (d) {
                 $.ylbAjaxHandler(d, function () {
-                    taste.list = d.data.customers;
+                    taste.clist = d.data.customerManagers;
                     m.buildVue();
                 });
             });
         },
         am: function () {
             $.when($.ajax({
-                url: $.apiUrl + "/area/customermanager",
+                url: $.apiUrl + "/area/customermanager?cp=1&sz=1000",
                 type: "GET"
             })).done(function (d) {
                 $.ylbAjaxHandler(d, function () {
-                    taste.list = d.data.customers;
-                    m.buildVue();
+                    taste.clist = d.data.customerManagers;
+                    $.when($.ajax({
+                        url: $.apiUrl + "/merchant/users?cp=1&sz=1000",
+                        type: "GET"
+                    })).done(function (d) {
+                        $.ylbAjaxHandler(d, function () {
+                            taste.olist = d.data.users;
+                            m.buildVue();
+                        });
+                    });
                 });
             });
         },
         cm: function () {
             $.when($.ajax({
-                url: $.apiUrl + "/area/customermanager",
+                url: $.apiUrl + "/customermanager/merchants?cp=1&sz=1000",
                 type: "GET"
             })).done(function (d) {
                 $.ylbAjaxHandler(d, function () {
-                    taste.list = d.data.customers;
-                    m.buildVue();
+                    taste.mlist = d.data.merchants;
+                    $.when($.ajax({
+                        url: $.apiUrl + "/merchant/users?cp=1&sz=1000",
+                        type: "GET"
+                    })).done(function (d) {
+                        $.ylbAjaxHandler(d, function () {
+                            taste.olist = d.data.users;
+                            m.buildVue();
+                        });
+                    });
                 });
             });
         },
@@ -108,9 +155,75 @@
                 el: "#taste-main",
                 data: taste,
                 methods: {
-                    
+                    toc: function (el) {
+                        var cf = confirm("确定要赠送体验名额？");
+                        if (cf) {
+                            var id = $(el.target).attr("data-id");
+                            var c = $(el.target).prev("input").val();
+                            if (!c) {
+                                $.ylbAlert("请输入赠送数量");
+                                return;
+                            }
+                            $.ajax({
+                                url: $.apiUrl + "/customermanager/quota",
+                                type: "PUT",
+                                data: JSON.stringify({
+                                    receiverID: id,
+                                    quota: c
+                                })
+                            }).done(function (d) {
+                                $.ylbAjaxHandler(d, function () {
+                                    $.ylbAlert("发送成功");
+                                    m.getCount();
+                                });
+                            })
+                        }
+                    },
+                    tom: function (el) {
+                        var cf = confirm("确定要赠送体验名额？");
+                        if (cf) {
+                            var id = $(el.target).attr("data-id");
+                            var c = $(el.target).prev("input").val();
+                            if (!c) {
+                                $.ylbAlert("请输入赠送数量");
+                                return;
+                            }
+                            $.ajax({
+                                url: $.apiUrl + "/merchant/quota",
+                                type: "PUT",
+                                data: JSON.stringify({
+                                    receiverID: id,
+                                    quota: c
+                                })
+                            }).done(function (d) {
+                                $.ylbAjaxHandler(d, function () {
+                                    $.ylbAlert("发送成功");
+                                    m.getCount();
+                                });
+                            })
+                        }
+                    },
+                    too: function (el) {
+                        var cf = confirm("确定要赠送体验积分？");
+                        if (cf) {
+                            var id = $(el.target).attr("data-id");
+                            $.ajax({
+                                url: $.apiUrl + "/user/quota",
+                                type: "PUT",
+                                data: JSON.stringify({
+                                    receiverID: id
+                                })
+                            }).done(function (d) {
+                                $.ylbAjaxHandler(d, function () {
+                                    $.ylbAlert("发送成功");
+                                    m.getCount();
+                                });
+                            })
+                        }
+                    }
+
                 }
-            })
+            });
         }
     };
     m.init()
