eb90f4b93576081f400d433148b8d7ef3d72a527
2558772316 <2558772316@qq.com>
2016-09-16 23:58:56 +0800

    config page done

--- admin/build/js/config.js
+++ admin/build/js/config.js
@@ -8,7 +8,7 @@
         },
         getConfig: function () {
             $.when($.ajax({
-                url: $.apiUrl + "/config",
+                url: $.apiUrl + "/configs",
                 type: "GET"
             })).done(function (d) {
                 $.ylbAjaxHandler(d, function () {
@@ -22,8 +22,24 @@
                 el: "#config-main",
                 data: config,
                 methods: {
-                    editable: function () {
-                        config.isedit = !config.isedit;
+                    edit: function (el) {
+                        var _this = $(el.target);
+                        var k = _this.attr("data-key");
+                        var v = _this.attr("data-value");
+                        var c = confirm("确定要设置该项？");
+                        if (c) {
+                            $.ajax({
+                                url: $.apiUrl + "/config",
+                                type: "POST",
+                                data: JSON.stringify({
+                                    key: k,
+                                    value: v
+                                })
+                            }).done(function (d) {
+                                $.ylbAlert("设置成功！");
+                                m.getConfig();
+                            })
+                        }
                     }
                 }
             })
