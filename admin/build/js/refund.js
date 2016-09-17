3a19af8932040f29eb0a3f0a8bb7b89d5eafce40
2558772316 <2558772316@qq.com>
2016-09-17 11:27:47 +0800

    refund all

---
+++ admin/build/js/refund.js
@@ -0,0 +1,132 @@
+(function () {
+    var refund = {
+        filter: {
+            m: "",
+            en: "",
+            st: "",
+            et: ""
+        },
+        cp: 1,
+        sz: 25,
+        t: 0,
+        oid: "",
+        note: "",
+        gnote: false,
+        cover: false
+    };
+    var m = {
+        init: function () {
+            m.getList();
+        },
+        getList: function () {
+            $.when($.ajax({
+                url: $.apiUrl + "/order/refund?k=0&cp=1&sz=" + refund.sz,
+                type: "GET"
+            })).done(function (d) {
+                console.log(d);
+                $.ylbAjaxHandler(d, function () {
+                    refund.list = d.data;
+                    m.buildVue();
+                });
+            });
+        },
+        filterList: function () {
+            $.when($.ajax({
+                url: $.apiUrl + "/order/refund?k=0&m=" + refund.filter.m + "&et=" + refund.filter.et + "&st=" + refund.filter.st + "&en=" + refund.filter.en + "&cp=" + refund.cp + "&sz=" + refund.sz,
+                type: "GET"
+            })).done(function (d) {
+                $.ylbAjaxHandler(d, function () {
+                    refund.list = d.data;
+                });
+            });
+        },
+        buildVue: function () {
+            refund = new Vue({
+                el: "#refund-main",
+                data: refund,
+                methods: {
+                    filterlist: function () {
+                        m.filterList();
+                    },
+                    shownote: function (id, note) {
+                        refund.oid = id;
+                        refund.note = note;
+                        refund.gnote = true;
+                        refund.cover = true;
+                    },
+                    addnote: function () {
+                        $.ajax({
+                            url: $.apiUrl + "/order/refund/note",
+                            type: "POST",
+                            data: JSON.stringify({
+                                refundOrderID: refund.oid,
+                                note: refund.note
+                            })
+                        }).done(function (d) {
+                            $.ylbAjaxHandler(d, function () {
+                                $.ylbAlert("添加成功！");
+                                m.filterList();
+                            })
+                        });
+                    },
+                    hideall: function () {
+                        refund.cover = false;
+                        refund.gnote = false;
+                    },
+                    agree: function (id) {
+                        var c = confirm("确认同意该笔退款？");
+                        if (c) {
+                            $.ajax({
+                                url: $.apiUrl + "/order/refund/agree",
+                                type: "POST",
+                                data: JSON.stringify({ refundOrderID: id })
+                            }).done(function (d) {
+                                $.ylbAjaxHandler(d, function () {
+                                    $.ylbAlert("操作成功！");
+                                    m.filterList();
+                                });
+                            });
+                        }
+                    },
+                    disagree: function (id) {
+                        var c = confirm("确认拒绝该笔退款？");
+                        if (c) {
+                            $.ajax({
+                                url: $.apiUrl + "/order/refund/reject",
+                                type: "POST",
+                                data: JSON.stringify({ refundOrderID: id })
+                            }).done(function (d) {
+                                $.ylbAjaxHandler(d, function () {
+                                    $.ylbAlert("操作成功！");
+                                    m.filterList();
+                                });
+                            });
+                        }
+                    },
+                    prev: function () {
+                        if (refund.cp <= 1) {
+                            return;
+                        } else {
+                            refund.cp = +refund.cp - 1;
+                            m.filterList();
+                        }
+                    },
+                    next: function () {
+                        if (refund.cp >= Math.ceil(refund.t / refund.sz)) {
+                            return;
+                        } else {
+                            refund.cp = +refund.cp + 1;
+                            m.filterList();
+                        }
+                    },
+                    jump: function () {
+                        if (refund.cp >= Math.ceil(refund.t / refund.sz)) refund.cp = Math.ceil(refund.t / refund.sz);
+                        if (refund.cp <= 1) refund.cp = 1;
+                        m.filterList();
+                    }
+                }
+            })
+        }
+    };
+    m.init();
+})();
